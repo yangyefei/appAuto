27b474f06e7e15ea5640ca2d3694b1af7cb7acdb
ALLen <ALLen@ALLen-PC>
2016-12-07 10:08:39 +0800

    test

---
+++ src/main/java/orm/MysqlDBName.java
@@ -0,0 +1,14 @@
+package orm;
+
+public class MysqlDBName {
+
+	/**
+	 * 短信
+	 */
+	public static final String XLH_SMS = "xlh_sms";
+
+	/**
+	 * 孵化器
+	 */
+	public static final String XLH_FHQ = "xlh_fhq";
+}
